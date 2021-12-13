8f86237b6e0da29f8a47f52684baa2f66e8fe4ed
MARIO ACEVEDO <JUN_ACEVEDO415@YAHOO.COM>
2021-12-13 12:31:54 -0800

    added more questions to about-me

--- js/app.js
+++ js/app.js
@@ -2,9 +2,21 @@
 
 console.log('hi');
 
+let username = prompt('what is your name?');
+console.log(username);
+alert ('Hello' +  username);
+
 let amIFromSanfrancisco = prompt('Am I from San Francisco? Yes or No?').
 toLowerCase();
 console.log(amIFromSanfrancisco);
+
+let where  = prompt ('What Part of San Francisco?')
+console.log(where);
+alert (' I am from there too!' + username);
+
+let favorite = prompt (' What is your favorite thing to do there?')
+console.log(favorite);
+alert ('that sounds like fun!' + username);
 
 //let lowerCaseSanfrancisco = amIFromSanfrancisco.toLowerCase();
 //console.log(lowerCaseSanfrancisco);
@@ -17,9 +29,15 @@ if (amIFromSanfrancisco === 'yes' || amIFromSanfrancisco === 'y') {
   console.log('please answer with a yes or no');
 }
 
-let username = prompt('what is your name?');
-console.log(username);
-alert ('Hello' + username);
+//if(whatPartOfSanFrancisco  === 'Mission District'
+//document.write('Me too!');
+ //else {
+    //document.write('oh ok');
+//}
+
+
+
+
 
 
 
